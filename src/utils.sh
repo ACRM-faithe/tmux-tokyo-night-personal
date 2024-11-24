10992f40200219c00eb41ca5b7b933eeb30bf547
Sebastian <z5479865@ad.unsw.edu.au>
2024-11-24 17:27:34 +1100

    investingating getting white text

--- src/utils.sh
+++ src/utils.sh
@@ -39,7 +39,8 @@ function generate_active_window_string() {
 		left_separator_inverse=$(get_tmux_option "@theme_transparent_left_separator_inverse" "")
 
 		local separator_start="#[bg=default,fg=${PALLETE['dark5']}]${left_separator_inverse}#[bg=${PALLETE['dark5']},fg=${PALLETE['bg_highlight']}]"
-		local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['dark5']}]${left_separator:?}#[none]"
+		local separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
+		#local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['dark5']}]${left_separator:?}#[none]"
 		local separator_end="#[bg=default,fg=${PALLETE['dark3']}]${left_separator:?}#[none]"
 	else
 		local separator_start="#[bg=${PALLETE['dark5']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
@@ -62,13 +63,13 @@ function generate_inactive_window_string() {
 	if [ "$transparent" = "true" ]; then
 		left_separator_inverse=$(get_tmux_option "@theme_transparent_left_separator_inverse" "")
 		
-		separator_start="#[bg=default,fg=${PALLETE['magenta']}]${left_separator_inverse}#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]"
-		separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
-		separator_end="#[bg=default,fg=${PALLETE['purple']}]${left_separator:?}#[none]"
+		local separator_start="#[bg=default,fg=${PALLETE['magenta']}]${left_separator_inverse}#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]"
+		local separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
+		local separator_end="#[bg=default,fg=${PALLETE['purple']}]${left_separator:?}#[none]"
 	else
-		separator_start="#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
-		separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
-		separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['purple']}]${left_separator:?}#[none]"
+		local separator_start="#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
+		local separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
+		local separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['purple']}]${left_separator:?}#[none]"
 	fi
 
 	echo "${separator_start}#[fg=${PALLETE[white]}]${separator_internal}#I#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$active_window_icon}${active_window_title}#{?pane_synchronized,$pane_synchronized_icon,}${separator_end}#[none]"
